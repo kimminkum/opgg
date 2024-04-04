d432963f2be0af76b72e8d496cd2c4af5c229b96
kimminkum <100018239+kimminkum@users.noreply.github.com>
2024-04-04 20:14:54 +0900

    cnlwlrgka

--- src/components/MainAni.tsx
+++ src/components/MainAni.tsx
@@ -1,5 +1,11 @@
 const MainAni: React.FC = () => {
-  return <div>메인 검색란에 들어갈 애니메이션 하나 만들기.</div>;
+  return (
+    <div>
+      메인 검색란에 들어갈 애니메이션 하나 만들기.
+      <br />
+      취직함 3.18
+    </div>
+  );
 };
 
 export default MainAni;
