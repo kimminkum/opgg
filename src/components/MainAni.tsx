067e35f1d92b37fae363710c5d4a4f8551a39c32
kimminkum <100018239+kimminkum@users.noreply.github.com>
2024-04-20 20:06:15 +0900

    aws

--- src/components/MainAni.tsx
+++ src/components/MainAni.tsx
@@ -5,7 +5,10 @@ const MainAni: React.FC = () => {
       <br />
       취직함 3.18
       <br />
-      AWS ing - password + google autho
+      AWS ing - password + google autho / EC2 - VPC 연동? 저번에 했던걸로 기억함
+      내가 그럼 EC2에 IP추가 했으니 VPC도 자동으로 IP추가 된거 ? SSH 접속 RDS
+      데이터 베이스 생성을 내가 해야하는 것인지 ? 저번에 DB연결을 못했던거지
+      DB관련 뭐 했던걸로 기억하는데 EC2에서 RDS
     </div>
   );
 };
